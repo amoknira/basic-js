eafc46778741e7988ca85ed08f3fc7ecb66bb553
amoknira <keyforgen@gmail.com>
2021-10-02 22:21:09 +0300

    feat: carbon-dating solving

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+export default function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let constantK = 0.693/HALF_LIFE_PERIOD; // the half-life of a first-order reaction:t_0.5 = 0.693/k
+
+  if (typeof(sampleActivity) !== 'string' || sampleActivity <= 0 || sampleActivity >= MODERN_ACTIVITY || isNaN(sampleActivity)) {
+    return false;
+  }
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / constantK); //as values of MODERN_ACTIVITY and sampleActivity are natural logarithms  N0/(N*k)
 }
