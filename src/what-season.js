ba43bbea529d4d72ad6f244961543b53408644ff
amoknira <keyforgen@gmail.com>
2021-10-02 22:38:56 +0300

    feat: what-season solving

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,34 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+export default function getSeason(date) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-}
+  if (!date){
+    return "Unable to determine the time of year!";
+  }
+  try {
+    date.toDateString();
+  } catch (error) {
+    throw new Error ("Invalid date!");
+  }
+    switch(date.getMonth()){
+      case 11: 
+      //cause 11 = december and 0=january
+      case 0:
+      case 1:
+        return "winter";
+      case 2:
+      case 3:
+      case 4:
+        return "spring";
+      case 5:
+      case 6:
+      case 7:
+        return "summer";
+      case 8:
+      case 9:
+      case 10:
+        return "fall";
+    }
+  }
